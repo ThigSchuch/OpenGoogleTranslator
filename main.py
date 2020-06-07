4b8add8f1888326f2592eca20ec14d742c64752a
ThigSchuch <thigschuch@gmail.com>
2020-06-07 11:16:55 -0300

    Better markdown and typos

--- main.py
+++ main.py
@@ -70,10 +70,10 @@ def checkLang(lang):
 def start(update, context):
     keepID(update.effective_user.id)
     person = update.effective_user.first_name
-    update.message.reply_text(
+    update.message.reply_markdown(
                             f"""Olá {person}, tudo bem? Vou te ajudar a traduzir as coisas!
-                            \nPrimeiro, vamos arrumar a casa, digite `/setLang` e a sigla para definir seu idioma padrão.
-                            \nExemplo: `/setLang pt`""", parse_mode="MARKDOWN")
+                            \nPrimeiro, vamos arrumar a casa, digite `/setLang` e a sigla para definir seu idioma nativo.
+                            \nExemplo: `/setLang pt`""")
 
 # Comando para definir o idioma nativo
 def setLang(update, context):
@@ -81,13 +81,13 @@ def setLang(update, context):
         lang = update.message.text.split(" ")[1].lower() # Se o comando foi certo, o idioma é este
         if checkLang(lang):
             updateDB(update.effective_user.id, lang, "toLang")
-            update.message.reply_text(f"""Certo! Seu idioma nativo é "{lang}".
-                                        \nAgora defina o inverso, vou usar ela quando você digitar em seu Idioma Nativo.
-                                        \nExemplo: `/setReverse en`""", parse_mode="MARKDOWN")
+            update.message.reply_markdown(f"""Certo! Seu idioma nativo é "{lang}".
+                                        \nAgora defina o reverso, vou usar ele quando você digitar em seu Idioma Nativo.
+                                        \nExemplo: `/setReverse en`""")
         else:
-            update.message.reply_text(f'Não encontrei essa língua "{lang}", você deve digitar a sigla.\nNão sabe qual? Clique aqui /langs')
+            update.message.reply_text(f'Não encontrei esse idioma "{lang}", você deve digitar a sigla.\nNão sabe qual? Clique aqui /langs')
     except:
-        update.message.reply_text('Você deve digitar `/setLang + idioma.`', parse_mode="MARKDOWN")
+        update.message.reply_mardown('Você deve digitar `/setLang + sigla do idioma.`')
 
 # Comando para definir o idioma reverso
 def setReverse(update, context):
@@ -95,12 +95,12 @@ def setReverse(update, context):
         lang = update.message.text.split(" ")[1].lower() # Se o comando foi certo, o idioma é este
         if checkLang(lang):
             updateDB(update.effective_user.id, lang, "reverseLang")
-            update.message.reply_text(f"""Certo! Seu idioma backup é "{lang}".
+            update.message.reply_text(f"""Certo! Seu idioma reverso é "{lang}".
                                         \nAgora você já pode mandar qualquer texto para mim.""")
         else:
-            update.message.reply_text(f'Não encontrei essa língua "{lang}", você deve digitar a sigla.\nNão sabe qual? Clique aqui /langs')
+            update.message.reply_text(f'Não encontrei esse idioma "{lang}", você deve digitar a sigla.\nNão sabe qual? Clique aqui /langs')
     except:
-        update.message.reply_text('Você deve digitar `/setReverse + idioma`.', parse_mode="MARKDOWN")
+        update.message.reply_markdown('Você deve digitar `/setReverse + sigla do idioma`.')
     
 # Qualquer texto enviado será traduzido
 def translate(update, context):
@@ -115,12 +115,12 @@ def translate(update, context):
     lang = translator.detect(text).lang # Detecta o idioma da mensagem
 
     if lang != userData[1]: # De qualquer idioma para o nativo
-        msg = f'{languages[lang]} detectado\n\n{translator.translate(text, dest=userData[1]).text}'
+        msg = f'{languages[lang]} detectado\n\n`{translator.translate(text, dest=userData[1]).text}`'
         
     else: # Do nativo para o reverse
-        msg = f'{languages[userData[2]]}\n\n{translator.translate(text, dest=userData[2]).text}'
+        msg = f'{languages[userData[2]]}\n\n`{translator.translate(text, dest=userData[2]).text}`'
     
-    update.message.reply_text(msg)
+    update.message.reply_markdown(msg)
 
 # Langs
 def langs(update, context):
@@ -135,28 +135,28 @@ def langs(update, context):
 def myInfo(update, context):
     userData = getUserInfo(update.effective_user.id)
 
-    msg = f"Tudo o que tenho de você é isto:\n\nSeu ID: {userData[0]}\nSua língua nativa: {userData[1]}\nE sua língua reversa: {userData[2]}"
+    msg = f"Tudo o que tenho de você é isto:\n\nSeu ID: `{userData[0]}`\nSeu idioma nativo: *{userData[1]}*\nSua idioma reverso: *{userData[2]}*"
     
-    update.message.reply_text(msg)
+    update.message.reply_markdown(msg)
 
 # Help
 def helpCommand(update, context):
     msg = """Olá, tudo bem? Precisa de uma mãozinha?
     
 `/setLang` - Defina seu Idioma Nativo
-`/setReverse` - Usarei para traduzir quando você digitar em sua língua nativa\n
+`/setReverse` - Usarei para traduzir quando você digitar em seu Idioma Nativo\n
 `/langs` - Veja os idiomas em que consigo traduzir
 `/myInfo` - Quem é você? Só sei seus idiomas e ID
 `/help` - É esta mensagem aqui
         
-Ajude no meu desenvolvimento!\nhttps://github.com/ThigSchuch/OpenGoogleTranslator"""
-    update.message.reply_text(msg, parse_mode="MARKDOWN")
+[Ajude no meu desenvolvimento!](https://github.com/ThigSchuch/OpenGoogleTranslator)"""
+    update.message.reply_markdown(msg)
     
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    updater = Updater(token='INSIRA SEU TOKEN AQUI', use_context=True)
+    updater = Updater(token='1INSIRA SEU TOKEN AQUI', use_context=True)
     dp = updater.dispatcher
 
     # Comandos
