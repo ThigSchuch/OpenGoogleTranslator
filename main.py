0bea4b4eff165467dc2d17a691cf08313fc94986
ThigSchuch <thigschuch@gmail.com>
2020-06-07 12:28:11 -0300

    I hate emojis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-import sqlite3
+# -*- coding: UTF-8 -*-
+import logging, re, sqlite3
 from googletrans import Translator
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -24,6 +24,18 @@ languages = {
     'tg':'Tajik','ta':'Tamil','te':'Telugu','th':'Thai','tr':'Turkish','uk':'Ukrainian','ur':'Urdu','uz':'Uzbek',
     'vi':'Vietnamese','cy':'Welsh','xh':'Xhosa','yi':'Yiddish','yo':'Yoruba','zu':'Zulu'
     }
+
+# Remove os emojis :)
+def deEmojify(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"
+                           u"\U0001F300-\U0001F5FF"
+                           u"\U0001F680-\U0001F6FF"
+                           u"\U0001F1E0-\U0001F1FF"
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
 
 # Realiza alterções das linguagens no BD
 def updateDB(userID, lang, option):
@@ -107,9 +119,9 @@ def translate(update, context):
     userData = getUserInfo(update.effective_user.id)
     
     if update.message.text != None: # Se for texto normal
-        text = update.message.text
+        text = deEmojify(update.message.text)
     else:
-        text = update.message.caption # Se for algo com legenda
+        text = deEmojify(update.message.caption) # Se for algo com legenda
 
     translator = Translator()
     lang = translator.detect(text).lang # Detecta o idioma da mensagem
@@ -156,7 +168,7 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    updater = Updater(token='1INSIRA SEU TOKEN AQUI', use_context=True)
+    updater = Updater(token='INSIRA SEU TOKEN AQUI', use_context=True)
     dp = updater.dispatcher
 
     # Comandos
